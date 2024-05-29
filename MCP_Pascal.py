58c6f52fe4d489a3134806988981e23cf727fd37
PascalPflueger <yourdearpascal1@gmx.de>
2024-05-29 23:21:44 +0200

    update

---
+++ MCP_Pascal.py
@@ -0,0 +1,21 @@
+# just some libs
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import pandas as pd
+import xarray as xr
+import pandas as pd
+import os
+import netCDF4 as nc
+from netCDF4 import Dataset
+import numpy as np
+from scipy.stats import linregress
+
+##--------------
+# refrence to "Long-term correction of wind observations by diffusion-based transformation"
+# Page 19
+##--------------
+
+
+##-------------- Linear regression
+
--- test.py
+++
@@ -1,3 +0,0 @@
-import matplotlib
-
-print(1)
